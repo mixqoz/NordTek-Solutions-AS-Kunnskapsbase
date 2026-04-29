8fba54d1fabc6b6f5159d75f57b2464ed595eb52
mixqoz <unknownmixqo@gmail.com>
2026-04-29 17:19:59 +0200

    Set up required files + navbar
    All basic files with professional styling for IT + compatible navbar

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hjem():
+	return render_template('index.html')
+
+@app.route('/lovverk')
+def lovverk():
+	return render_template('lovverk.html')
+
+@app.route('/kundesupport')
+def kundesupport():
+	return render_template('kundesupport.html')
+
+@app.route('/artikkler')
+def artikkler():
+	return render_template('artikkler.html')
+
+
+if __name__ == '__main__':
+	app.run(debug=True)
