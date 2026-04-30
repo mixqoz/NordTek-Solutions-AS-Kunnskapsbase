c6ab448a6b94224facdeeaafe449b787d30e0409
mixqoz <unknownmixqo@gmail.com>
2026-04-30 13:01:08 +0200

    Finalize touches

--- app.py
+++ app.py
@@ -21,6 +21,5 @@ def kundesupport():
 def artikkler():
 	return render_template('artikkler.html')
 
-
-if __name__ == '__main__':
-	app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
